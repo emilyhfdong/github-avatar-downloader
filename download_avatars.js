915c0df33b5538ba926a31ed7d2e336f62969dfc
Emily Dong <emilyhfdong@gmail.com>
2018-06-11 18:21:33 +0000

    modified getRepoContributors function to print the URL of avatars

--- download_avatars.js
+++ download_avatars.js
@@ -4,20 +4,28 @@ var secrets = require('./secrets.js')
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
+
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization': secrets.GITHUB_TOKEN;
+      'Authorization': secrets.GITHUB_TOKEN
     }
   };
 
   request(options, function(err, res, body) {
-    cb(err, body);
+    var data = JSON.parse(body)
+    cb(err, data);
+
   });
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors("jquery", "jquery", function(err, data) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  console.log("Avatar URLs: ");
+
+  for (var i = 0; i < data.length; i ++ ) {
+    console.log(data[i].avatar_url);
+  }
+
 });
