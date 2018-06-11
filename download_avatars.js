5f22af03cadf53ccc1395b0dcc779d310b91bc73
Emily Dong <emilyhfdong@gmail.com>
2018-06-11 18:48:05 +0000

    connected the functions

--- download_avatars.js
+++ download_avatars.js
@@ -3,44 +3,39 @@ var secrets = require('./secrets.js');
 var fs = require('fs');
 
 
-// console.log('Welcome to the GitHub Avatar Downloader!');
+console.log('Welcome to the GitHub Avatar Downloader!');
 
-// function getRepoContributors(repoOwner, repoName, cb) {
+function getRepoContributors(repoOwner, repoName) {
 
-//   var options = {
-//     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-//     headers: {
-//       'User-Agent': 'request',
-//       'Authorization': secrets.GITHUB_TOKEN
-//     }
-//   };
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': secrets.GITHUB_TOKEN
+    }
+  };
 
-//   request(options, function(err, res, body) {
-//     var data = JSON.parse(body)
-//     cb(err, data);
+  request(options, function(err, res, body) {
+    var data = JSON.parse(body);
+    for (var i = 0; i < data.length; i ++ ) {
+      var filePath = './avatars/' + data[i].login + '.jpg';
+      var avatarURL = data[i].avatar_url;
+      downloadImageByURL(avatarURL, filePath);
+    }
 
-//   });
-// }
+  });
+}
 
 function downloadImageByURL(url, filePath) {
-
   request.get(url)
        .on('error', function (err) {
          throw err;
        })
-       .pipe(fs.createWriteStream('./' + filePath));
+       .pipe(fs.createWriteStream(filePath));
 
 }
 
-downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
+// downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
 
-// getRepoContributors("jquery", "jquery", function(err, data) {
-//   console.log("Errors:", err);
-//   console.log("Avatar URLs: ");
-
-//   for (var i = 0; i < data.length; i ++ ) {
-//     console.log(data[i].avatar_url);
-//   }
-
-// });
+getRepoContributors("jquery", "jquery");
 
