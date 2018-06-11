1eba0c899558099c8a2a31f0e0408234a216991e
Emily Dong <emilyhfdong@gmail.com>
2018-06-11 18:38:29 +0000

    added function to download image by url

--- download_avatars.js
+++ download_avatars.js
@@ -1,31 +1,46 @@
 var request = require('request');
-var secrets = require('./secrets.js')
+var secrets = require('./secrets.js');
+var fs = require('fs');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb) {
+// console.log('Welcome to the GitHub Avatar Downloader!');
 
-  var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-    headers: {
-      'User-Agent': 'request',
-      'Authorization': secrets.GITHUB_TOKEN
-    }
-  };
+// function getRepoContributors(repoOwner, repoName, cb) {
 
-  request(options, function(err, res, body) {
-    var data = JSON.parse(body)
-    cb(err, data);
+//   var options = {
+//     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+//     headers: {
+//       'User-Agent': 'request',
+//       'Authorization': secrets.GITHUB_TOKEN
+//     }
+//   };
 
-  });
+//   request(options, function(err, res, body) {
+//     var data = JSON.parse(body)
+//     cb(err, data);
+
+//   });
+// }
+
+function downloadImageByURL(url, filePath) {
+
+  request.get(url)
+       .on('error', function (err) {
+         throw err;
+       })
+       .pipe(fs.createWriteStream('./' + filePath));
+
 }
 
-getRepoContributors("jquery", "jquery", function(err, data) {
-  console.log("Errors:", err);
-  console.log("Avatar URLs: ");
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
 
-  for (var i = 0; i < data.length; i ++ ) {
-    console.log(data[i].avatar_url);
-  }
+// getRepoContributors("jquery", "jquery", function(err, data) {
+//   console.log("Errors:", err);
+//   console.log("Avatar URLs: ");
 
-});
+//   for (var i = 0; i < data.length; i ++ ) {
+//     console.log(data[i].avatar_url);
+//   }
+
+// });
+
